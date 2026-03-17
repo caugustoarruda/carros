3f4ead0616642ef0ae9cf87577d0ac7162d79ef4
Carlos Arruda <caugustogarruda@gmail.com>
2026-03-17 18:32:35 -0300

    Adiciona placa e foto no model, adiciona media root e url no settings e extende static media na url

--- cars/models.py
+++ cars/models.py
@@ -12,10 +12,11 @@ class Car(models.Model):
     id = models.AutoField(primary_key=True)
     model = models.CharField(max_length=200)
     brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name='car_brand')
-    # brand = models.CharField(max_length=200)
     factory_year = models.IntegerField(blank=True, null=True)
     model_year = models.IntegerField(blank=True, null=True)
+    plate = models.CharField(max_length=10, blank=True, null=True)
     value = models.FloatField(blank=True, null=True)
+    photo = models.ImageField(upload_to='cars/', blank=True, null=True)
 
     def __str__(self):
         return self.model
