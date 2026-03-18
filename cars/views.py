0f39a51fa7b7296eceb136d09499a5026f2059eb
Carlos Arruda <caugustogarruda@gmail.com>
2026-03-18 18:27:56 -0300

    Retornando html puro direto na view

--- cars/views.py
+++ cars/views.py
@@ -1,4 +1,14 @@
 from django.http import HttpResponse
 
 def cars_view(request):
-    return HttpResponse("Novos carros")
+    html = """
+        <html>
+            <head>
+                <title>Carros novos</title>
+            </head>
+            <body>
+                <h1>Aqui voce encontra os carros mais novos da região</h1>
+            </body>
+        </html>
+    """
+    return HttpResponse(html)
