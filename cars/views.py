542382d5edfef3f0d64202c34ac2bdd0e9180034
Carlos Arruda <caugustogarruda@gmail.com>
2026-03-20 19:23:07 -0300

    Renderiza listagem de carros direto do model

--- cars/views.py
+++ cars/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from cars.models import Car
 
 def cars_view(request):
+    cars = Car.objects.all()
     return render(
         request=request, 
         template_name='cars.html',
-        context={'cars': {'model': 'Argo 1.0'}}
+        context={'cars': cars}
     )
