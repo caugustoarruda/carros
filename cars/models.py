9160a1f11d1a31bc4dc2029a902e927c1d10d1e1
Carlos Arruda <caugustogarruda@gmail.com>
2026-04-03 15:02:14 -0300

    Cria tabela de inventario

--- cars/models.py
+++ cars/models.py
@@ -20,3 +20,11 @@ class Car(models.Model):
 
     def __str__(self):
         return self.model
+    
+class CarInventory(models.Model):
+    cars_count = models.IntegerField()
+    cars_value = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.cars_count} - {self.cars_value}'
