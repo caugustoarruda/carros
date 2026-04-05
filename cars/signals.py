7e4c9ea282f721a960a4d07dd2fc92676ff4746e
Carlos Arruda <caugustogarruda@gmail.com>
2026-04-05 12:14:56 -0300

    Atualizando inventario com signals

--- cars/signals.py
+++ cars/signals.py
@@ -1,19 +1,23 @@
-from django.db.models.signals import pre_save, post_save, pre_delete, post_delete
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
-from cars.models import Car
+from django.db.models import Sum
+from cars.models import Car, CarInventory
 
-@receiver(pre_save, sender=Car)
-def car_pre_save(sender, instance, **kwargs):
-    print(f'Quantidade {len(sender.objects.all())}')
+
+def car_invetory_update():
+    cars_count = Car.objects.all().count()
+    cars_value = Car.objects.aggregate(
+        total_value=Sum('value')
+    )['total_value']
+    CarInventory.objects.create(
+        cars_count=cars_count,
+        cars_value=cars_value
+    )
 
 @receiver(post_save, sender=Car)
 def car_post_save(sender, instance, **kwargs):
-    print(f'Quantidade {len(sender.objects.all())}')
-
-@receiver(pre_delete, sender=Car)
-def car_post_save(sender, instance, **kwargs):
-    print(f'Quantidade {len(sender.objects.all())}')
+    car_invetory_update()
 
 @receiver(post_delete, sender=Car)
 def car_post_delete(sender, instance, **kwargs):
-    print(f'Quantidade {len(sender.objects.all())}')
+    car_invetory_update()
